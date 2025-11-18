0180201edc06361ee511e6bd7423bea80921e5fa
Derek <dereksturm263@gmail.com>
2025-11-18 15:24:57 +0000

    fixed weather code. Need to test on machine

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,53 +1,43 @@
 import { get_next_weather, set_next_weather, set_current_weather    } from '../database.js';	
 import weathers from '../exports/weathers.js';
 
+function createWeather(now) {
+    const randomIndex = Math.floor(Math.random() * weathers.length);
+    const weather = weathers[randomIndex];
+    
+    weather.start_time = now.getTime();
+    if (weather.cooldown > 0)
+        weather.cooldown += Math.floor(Math.random() * 4) - 2;
+
+    weather.end_time = now.getTime() + (2 * 60 * 60 * 1000);
+
+    return weather;
+}
+
 export const event = {
     name: 'clientReady',
     async execute(client) {
         console.log(`${client.user.tag} is online.`);
 
-        const now = new Date();
-        const later = new Date(now.getTime() + (2 * 60 * 60 * 1000));
-        const evenLater = new Date(later.getTime() + (2 * 60 * 60 * 1000))
+        const currentWeather = createWeather(new Date());
+        const nextWeather = createWeather(new Date(currentWeather.start_time + (2 * 60 * 60 * 1000)));
 
-        // Set default weather.
-        let randomIndex = Math.floor(Math.random() * weathers.length);
-        let weather = weathers[randomIndex];
-        weather.start_time = now.getTime();
-        if (weather.cooldown > 0)
-            weather.cooldown += Math.floor(Math.random() * 4) - 2;
+        await set_current_weather(currentWeather);
+        await set_next_weather(nextWeather);
 
-        weather.end_time = later.getTime();
-
-        let randomIndex2 = Math.floor(Math.random() * weathers.length);
-        let weather2 = weathers[randomIndex2];
-        weather2.start_time = later.getTime();
-        if (weather2.cooldown > 0)
-            weather2.cooldown += Math.floor(Math.random() * 4) - 2;
-
-        weather2.end_time = evenLater.getTime();
-        
-        await set_current_weather(weather);
-        await set_next_weather(weather2);
-        client.user.setActivity(weather.name);
-        console.log(`Weather set to ${weather.id}.`);
+        client.user.setActivity(currentWeather.name);
+        console.log(`Weather set to ${currentWeather.id}.`);
 
         // Update the weather every 2 hours.
         setInterval(() => async () => {
-            randomIndex = Math.floor(Math.random() * weathers.length);
-            weather = weathers[randomIndex];
-    
-            weather.start_time = now.getTime();
-            if (weather.cooldown > 0)
-                weather.cooldown += Math.floor(Math.random() * 4) - 2;
+            const newCurrentWeather = await get_next_weather();
+            const newNextWeather = createWeather(new Date(newCurrentWeather.start_time + (2 * 60 * 60 * 1000)));
 
-            weather.end_time = later.getTime();
+        	await set_current_weather();
+            await set_next_weather(newNextWeather);
 
-        	await set_current_weather(await get_next_weather());
-            await set_next_weather(weather);
-            client.user.setActivity(weather.name);
-
-            console.log(`Weather set to ${weather.name}.`);
+            client.user.setActivity(newCurrentWeather.name);
+            console.log(`Weather set to ${newCurrentWeather.name}.`);
         }, 2 * 60 * 60 * 1000);
     }
 };
