ccfd503ad560956f086dde7f15fa2e94ead3abe2
DerekSturm263 <dereksturm263@gmail.com>
2025-11-24 17:46:41 -0800

    a

--- src/commands/collect.js
+++ src/commands/collect.js
@@ -43,11 +43,8 @@ export const command = {
 
 		// Check if the user isn't ready to collect more snow.
 		if (user_data.ready_time > new Date().getTime()) {
-			const difference = user_data.ready_time - new Date().getTime();
-			const seconds = Math.ceil(difference / 1000);
-
 			await interaction.editReply({
-				content: `You have to wait ${seconds} more second${seconds == 1 ? '' : 's'} before you can collect more snow!`,
+				content: `Slow down! You can collect more snow <t:${user_data.ready_time}:R>.`,
 				flags: MessageFlags.Ephemeral
 			});
 			return;
