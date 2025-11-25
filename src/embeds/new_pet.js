1f31c06872cdf06eaffecdab913291e6fbfcd916
DerekSturm263 <dereksturm263@gmail.com>
2025-11-24 18:01:38 -0800

    a

--- src/embeds/new_pet.js
+++ src/embeds/new_pet.js
@@ -5,7 +5,7 @@ export function build_new_pet(pet, isEgg) {
 		.setColor(0xFFFFFF)
 		.setTitle(`${isEgg ? 'Unhatched Egg' : pet.name}`)
         .setDescription(`${isEgg ? `This egg will hatch **<t:${Math.floor(pet.hatch_time / 1000)}:R>**.` : pet.descriptions[Math.floor(pet.total_food / 20)]}`)
-		.setImage(pet.image)
+		.setImage(isEgg ? "https://images.everydayhealth.com/images/news/an-egg-day-lower-dementia-risk-1440x810.jpg?sfvrsn=d81b2e39_3" : pet.image)
 		.addFields(
 			{
 				name: 'Level',
