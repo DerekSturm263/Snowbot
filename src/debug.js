5b45297cac03bad83ebc3edbbe8463ea02b663f9
DerekSturm263 <dereksturm263@gmail.com>
2025-11-24 23:44:23 -0800

    e

--- src/debug.js
+++ src/debug.js
@@ -1,14 +1,14 @@
-import { writeFileSync } from 'fs';
+import { appendFileSync } from 'fs';
 
 const filePath = 'debug.txt';
 
 export default function log(message) {
     console.log(message);
-    writeFileSync(filePath, message);
+    appendFileSync(filePath, message, );
 }
 
 export function logError(message) {
     console.logError(message);
-    writeFileSync(filePath, message);
+    appendFileSync(filePath, message);
 }
 
