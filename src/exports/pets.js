0779e4a0c50d0268f6ea4bbc6042f0e7fdfac250
DerekSturm263 <dereksturm263@gmail.com>
2025-11-24 21:38:58 -0800

    a

--- src/exports/pets.js
+++ src/exports/pets.js
@@ -5,7 +5,9 @@ const snow_worm = {
     descriptions: [
         "A tiny snow worm that does absolutely nothing.",
         "A tiny snow worm that still does absolutely nothing.",
-        "A tiny snow worm that somehow still does absolutely nothing."
+        "A tiny snow worm that somehow still does absolutely nothing.",
+        "A tiny snow worm that might actually do something...",
+        "Just kidding, the tiny snow worm doesn't do a thing."
     ],
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8yHASr5GITzfox_P7ACtyh_vJ_-yXPV-4pw&s",
     hatch_time: 0,
@@ -23,6 +25,8 @@ const snowman = {
     descriptions: [
         "A friendly snowman that makes itself useful by automatically collecting snow for you at a slow pace.",
         "A friendly snowman that makes itself useful by automatically collecting snow for you at a decent pace.",
+        "A friendly snowman that makes itself useful by automatically collecting snow for you at a quick pace.",
+        "A friendly snowman that makes itself useful by automatically collecting snow for you at a quick pace.",
         "A friendly snowman that makes itself useful by automatically collecting snow for you at a quick pace."
     ],
     image: "https://t3.ftcdn.net/jpg/06/62/11/08/360_F_662110844_RuQiE5nvuTntDQ0NJCGg89VMf5vkMmA5.jpg",
@@ -41,6 +45,8 @@ const snow_dog = {
     descriptions: [
         "A hyper snow dog that helps you out by occasionally attacking your target and dealing 2 extra damage.",
         "A hyper snow dog that helps you out by sometimes attacking your target and dealing 3 extra damage.",
+        "A hyper snow dog that helps you out by often attacking your target and dealing 4 extra damage.",
+        "A hyper snow dog that helps you out by often attacking your target and dealing 4 extra damage.",
         "A hyper snow dog that helps you out by often attacking your target and dealing 4 extra damage."
     ],
     image: "https://media.istockphoto.com/id/97875216/photo/samoyed-puppy-running-in-the-snow.jpg?s=612x612&w=0&k=20&c=6OzpAULdvcJMeYdUBUC76gr2k_l1J2_tMnds4DcAwt8=",
@@ -59,6 +65,8 @@ const snow_cat = {
     descriptions: [
         "A curious snow cat that assists you by occasionally finding random objects for you to pack in your snowball.",
         "A curious snow cat that assists you by sometimes finding random objects for you to pack in your snowball.",
+        "A curious snow cat that assists you by regularly finding random objects for you to pack in your snowball.",
+        "A curious snow cat that assists you by regularly finding random objects for you to pack in your snowball.",
         "A curious snow cat that assists you by regularly finding random objects for you to pack in your snowball."
     ],
     image: "https://t3.ftcdn.net/jpg/00/38/71/14/360_F_38711492_nrVsdh9rhkrDwZhRGVd5ipndrr97nikR.jpg",
@@ -77,6 +85,8 @@ const snow_wolf = {
     descriptions: [
         "A cunning snow wolf that makes itself useful by shortening the cooldown on your collection speed by a bit.",
         "A cunning snow wolf that makes itself useful by shortening the cooldown on your collection speed by a decent amount.",
+        "A cunning snow wolf that makes itself useful by shortening the cooldown on your collection speed by a lot.",
+        "A cunning snow wolf that makes itself useful by shortening the cooldown on your collection speed by a lot.",
         "A cunning snow wolf that makes itself useful by shortening the cooldown on your collection speed by a lot."
     ],
     image: "https://c02.purpledshub.com/uploads/sites/62/2024/08/Arctic-wolf-facts-scaled.jpg",
@@ -95,6 +105,8 @@ const snow_bunny = {
     descriptions: [
         "A lucky snow bunny that does its part by increasing your luck a little bit when throwing snowballs.",
         "A lucky snow bunny that does its part by increasing your luck a good amount when throwing snowballs.",
+        "A lucky snow bunny that does its part by increasing your luck a lot when throwing snowballs.",
+        "A lucky snow bunny that does its part by increasing your luck a lot when throwing snowballs.",
         "A lucky snow bunny that does its part by increasing your luck a lot when throwing snowballs."
     ],
     image: "https://media.istockphoto.com/id/510955047/photo/white-snowshoe-hare-on-snow.jpg?s=612x612&w=0&k=20&c=7TafMOjjgAF9sLz37Qlh7ChkjaoJ7ch15P7nSt5_Og0=",
@@ -113,6 +125,8 @@ const snow_fox = {
     descriptions: [
         "A sly snow fox that helps out by occasionally jumping in the way of oncoming snowballs.",
         "A sly snow fox that helps out by sometimes jumping in the way of oncoming snowballs.",
+        "A sly snow fox that helps out by often jumping in the way of oncoming snowballs.",
+        "A sly snow fox that helps out by often jumping in the way of oncoming snowballs.",
         "A sly snow fox that helps out by often jumping in the way of oncoming snowballs."
     ],
     image: "https://t3.ftcdn.net/jpg/02/25/52/78/360_F_225527887_1r2EaZbOrvCttqUDCvnA6HB9exVMrLXj.jpg",
@@ -131,6 +145,8 @@ const snow_owl = {
     descriptions: [
         "A helpful snow owl that does its part by repairing damage to your buildings slowly over time.",
         "A helpful snow owl that does its part by repairing damage to your buildings moderately over time.",
+        "A helpful snow owl that does its part by repairing damage to your buildings quickly over time.",
+        "A helpful snow owl that does its part by repairing damage to your buildings quickly over time.",
         "A helpful snow owl that does its part by repairing damage to your buildings quickly over time."
     ],
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj1oc2ZIaT4rbdQEiSnN5SVfMH2Zi0LSUifA&s",
@@ -147,6 +163,8 @@ const snow_dragon = {
     type: "snow_dragon",
     name: "Snow Dragon",
     descriptions: [
+        "A fierce snow dragon whose ability will be determined later.",
+        "A fierce snow dragon whose ability will be determined later.",
         "A fierce snow dragon whose ability will be determined later.",
         "A fierce snow dragon whose ability will be determined later.",
         "A fierce snow dragon whose ability will be determined later."
