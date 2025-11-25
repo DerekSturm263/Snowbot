39819245de5f9c466ebf32242791ecc425ac9c84
DerekSturm263 <dereksturm263@gmail.com>
2025-11-24 20:37:11 -0800

    r

--- src/embeds/new_collect.js
+++ src/embeds/new_collect.js
@@ -4,7 +4,7 @@ export function build_new_collect(amount, readyTime) {
 	return new EmbedBuilder()
 		.setColor(0xFFFFFF)
 		.setTitle('You collected some snow!')
-		.setDescription(`You can collect more in <t:${Math.floor(readyTime / 1000)}:R>`)
+		.setDescription(`You can collect more <t:${Math.floor(readyTime / 1000)}:R>`)
         .addFields({
 			name: 'Snow Collected',
 			value: `${amount}/20`
