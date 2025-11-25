c2cd8a0ecfee086a331802a9ce88af836f58af9c
DerekSturm263 <dereksturm263@gmail.com>
2025-11-24 23:45:12 -0800

    a

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,9 +4,7 @@ export const event = {
     name: 'clientReady',
     async execute(client) {
         log(`${client.user.tag} is online.`);
-        client.guilds.cache.forEach(guild => {
-            log(`- ${guild.name} (ID: ${guild.id})`);
-        });
+        log(`========================================================================`);
 
         client.user.setActivity(`/help | ${client.guilds.cache.size} servers`);
     }
