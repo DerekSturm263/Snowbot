aa07304839cc57c7d28a16498941864dd189165e
Derek <dereksturm263@gmail.com>
2025-12-01 01:53:53 -0800

    e

--- src/commands/throw.js
+++ src/commands/throw.js
@@ -170,7 +170,7 @@ export const command = {
 			// Update the building and tell the user it was hit.
 			await set_building(target.user.id, target_data.building);
 			await interaction.editReply({
-				embeds: [ build_snowball_block(target, server_data.buildings.find(item => item.id == target_data.building.id), target_data.data.building.hits_left) ]
+				embeds: [ build_snowball_block(target, server_data.buildings.find(item => item.id == target_data.building.id), target_data.building.hits_left) ]
 			});
 			
 			return;
