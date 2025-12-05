690d2e82deb6811568d84acc0f353461e602aa93
Derek <dereksturm263@gmail.com>
2025-12-04 23:06:03 -0800

    a

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,11 @@ import log from "../miscellaneous/debug.js";
 export const event = {
     name: 'clientReady',
     async execute(client) {
+        log('Servers the bot is in:');
+        client.guilds.cache.forEach(guild => {
+            log(`- ${guild.name} (ID: ${guild.id})`);
+        });
+
         log(`${client.user.tag} is online.`);
         log(`==================================================================================================================`);
 
